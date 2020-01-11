522c7a2fd9bb1ea43983566a716d9df68140e1dc
wifimedia <dungtd8x@gmail.com>
2020-01-11 15:45:20 +0700

    .

--- advance/files/controller.sh
+++ advance/files/controller.sh
@@ -212,8 +212,8 @@ cat $response_file | while read line ; do
 		value=$(echo $value | sed 's/,/ /g')
 		uci set wifimedia.@nodogsplash[0].preauthenticated_users="$value"
 	#Network
-	elif [  "$key" = "cpn.network" ];then
-	  uci set wifimedia.@nodogsplash[0].network="$value"		
+	#elif [  "$key" = "cpn.network" ];then
+	# uci set wifimedia.@nodogsplash[0].network="$value"		
 	elif [  "$key" = "cpn.fb" ];then
 		uci set wifimedia.@nodogsplash[0].facebook="$value"
 	elif [  "$key" = "cpn.dhcpextenal" ];then
