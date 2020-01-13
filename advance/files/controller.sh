386785f891e2addcea4b2cf9e263714b7e0a6e63
wifimedia <dungtd8x@gmail.com>
2020-01-13 14:05:44 +0700

    Make files
    touch /tmp/ports
    touch /tmp/client_connect_wlan

--- advance/files/controller.sh
+++ advance/files/controller.sh
@@ -6,6 +6,8 @@
 
 diag_file=/tmp/diagnostics_ip #store data ip diagnostics from server monitor
 touch $diag_file
+touch /tmp/ports
+touch /tmp/client_connect_wlan
 
 ip_public(){
 	PUBLIC_IP=`wget http://ipecho.net/plain -O - -q ; echo`
