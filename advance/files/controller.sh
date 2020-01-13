d3c44e902fc5725b100fb417372a8319b36100ee
wifimedia <dungtd8x@gmail.com>
2020-01-13 18:05:27 +0700

    Update controller.sh
    Fix DHCP LAN

--- advance/files/controller.sh
+++ advance/files/controller.sh
@@ -199,11 +199,11 @@ cat $response_file | while read line ; do
 		value=$(echo $value | sed 's/,/ /g')
 		uci set network.wan.dns="$value"		
 	##Cau hinh DHCP
-	elif [  "$key" = "lan.dhcp.start" ];then
+	elif [  "$key" = "network.dhcp.start" ];then
 		uci set dhcp.lan.start="$value"
-	elif [  "$key" = "lan.dhcp.limit" ];then
+	elif [  "$key" = "network.dhcp.limit" ];then
 		uci set dhcp.lan.limit="$value"
-	elif [  "$key" = "lan.dhcp.leasetime" ];then
+	elif [  "$key" = "network.dhcp.leasetime" ];then
 		uci set dhcp.lan.leasetime="$value"
 		
 	#Cau hinh Captive Portal
