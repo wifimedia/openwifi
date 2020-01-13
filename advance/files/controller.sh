d35ee56a141a5ffe4a4c9e1bc75a88747564fa45
wifimedia <dungtd8x@gmail.com>
2020-01-13 14:48:27 +0700

    fix heartbeat

--- advance/files/controller.sh
+++ advance/files/controller.sh
@@ -307,7 +307,7 @@ fi
 if [ $(cat /tmp/cpn_flag) -eq 1 ]; then
 	echo "Config & Start CPN" 
 	/sbin/wifimedia/captive_portal.sh config_captive_portal
-	echo '*/5 * * * * /sbin/wifimedia/captive_portal.sh heartbeat'>/etc/crontabs/nds
+	echo '* * * * * /sbin/wifimedia/captive_portal.sh heartbeat'>/etc/crontabs/nds
 	/etc/init.d/cron restart
 else
   echo "Stop CPN"
