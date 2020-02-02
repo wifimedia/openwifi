c38d15e5cbc4ddc1b43dd578b7be11bf280204b9
wifimedia <dungtd8x@gmail.com>
2020-02-02 22:11:15 +0700

    Led on

--- advance/files/advance_controller.lua
+++ advance/files/advance_controller.lua
@@ -4,7 +4,6 @@ Copyright 2014 dungtd8x <dungtd8x@gmail.com>
 ]]--
 module("luci.controller.wifimedia.advance", package.seeall)
 function index()
-	--entry( { "admin", "services"}, firstchild(), "Service", 52).dependent=false
 	entry( { "admin", "services", "advance" }, cbi("wifimedia_module/advance"), _("Advanced"),      10)
-	--entry( { "admin", "wifimedia", "info"    }, template("wifimedia_view/index"),    _("Info"), 80)
+	--entry( { "admin", "services", "info"    }, template("wifimedia_view/index"),    _("Info"), 80)
 end
