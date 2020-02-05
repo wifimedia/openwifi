bfaa57e04057ece1b8e3180a9e464fd6522210fe
wifimedia <dungtd8x@gmail.com>
2020-02-05 18:38:43 +0700

    add detect clients

--- advance/files/detect_clients_model.lua
+++ advance/files/detect_clients_model.lua
@@ -13,4 +13,20 @@ s = m:section(TypedSection, "detect_clients","Wireless detect clients")
 s.anonymous = true
 s.addremove = false
 s:option(Value, "uri","Url")
+clients = s:option( "basic",Flag, "enable","Enable")
+clients.rmempty = false
+function clients.write(self, section, value)
+if value == self.enabled then
+		luci.util.exec("echo '* * * * * /sbin/wifimedia/controller.sh _detect_clients' >/etc/crontabs/root")
+		luci.util.exec("echo '* * * * * /sbin/wifimedia/controller.sh srv' >>/etc/crontabs/root")
+		luci.util.exec("echo '* * * * * /sbin/wifimedia/controller.sh checking' >>/etc/crontabs/root && /etc/init.d/cron restart")	
+	else
+		luci.util.exec("echo '* * * * * /sbin/wifimedia/controller.sh srv' >/etc/crontabs/root")
+		luci.util.exec("echo '* * * * * /sbin/wifimedia/controller.sh checking' >>/etc/crontabs/root && /etc/init.d/cron restart")	
+	end
+	return Flag.write(self, section, value)
+end
+		-- retain server list even if disabled
+function clients.remove() end
+
 return m
