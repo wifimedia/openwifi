405dc0ac5dce8229f37430d60f4fda3d7c7bde81
wifimedia <dungtd8x@gmail.com>
2020-02-05 18:24:18 +0700

    add module detect clients

---
+++ advance/files/detect_clients_model.lua
@@ -0,0 +1,16 @@
+--[[
+LuCI - Lua Configuration Interface
+Copyright 2014 dungtd8x <dungtd8x@gmail.com>
+]]--
+
+local sys = require "luci.sys"
+local fs = require "nixio.fs"
+local uci = require "luci.model.uci".cursor()
+m = Map("wifimedia", "")
+m.apply_on_parse = true
+
+s = m:section(TypedSection, "detect_clients","Wireless detect clients")
+s.anonymous = true
+s.addremove = false
+s:option(Value, "api_url","api url")
+return m
