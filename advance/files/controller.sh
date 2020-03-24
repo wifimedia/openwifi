5212bbd2ca9673477431ec23da492ea28c4d98a6
wifimedia <dungtd8x@gmail.com>
2020-03-24 17:25:39 +0700

    Fix get info openvpn

--- advance/files/controller.sh
+++ advance/files/controller.sh
@@ -450,11 +450,11 @@ _get_server(){ # Connect to server Nextify
 }
 
 get_client_connect_wlan(){
-	_openvpn=`pidof openvpn`
-	if [ -n "$_openvpn" ];then
-		ip_opvn=`ifconfig tun0 | grep 'inet addr:' | cut -d: -f2 | awk '{ print $1 }'`
-	fi
-	local _url=$1
+	#_openvpn=`pidof openvpn`
+	#if [ -n "$_openvpn" ];then
+	#	ip_opvn=`ifconfig tun0 | grep 'inet addr:' | cut -d: -f2 | awk '{ print $1 }'`
+	#fi
+	#local _url=$1
 	NEWLINE_IFS='
 '
 	OLD_IFS="$IFS"; IFS=$NEWLINE_IFS
