80e6e471da5e9d650013c00faa8a65e5df55c425
wifimedia <dungtd8x@gmail.com>
2020-03-26 10:27:48 +0700

    move sw to  network

--- advance/files/switchmode_controller.lua
+++ advance/files/switchmode_controller.lua
@@ -4,5 +4,5 @@ Copyright 2014 dungtd8x <dungtd8x@gmail.com>
 ]]--
 module("luci.controller.wifimedia.switchmode", package.seeall)
 function index()
-	entry( { "admin", "services", "switchmode" }, cbi("wifimedia_module/switchmode"), _("Ethernet switch "),      15)
+	entry( { "admin", "network", "switchmode" }, cbi("wifimedia_module/switchmode"), _("Ethernet switch "),      15)
 end
