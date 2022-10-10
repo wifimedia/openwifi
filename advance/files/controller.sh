11dcb7063f2676721b92931ceeb23e5a1e39f416
wifimedia <dungtd8x@gmail.com>
2022-10-10 17:14:04 +0700

    ...

--- advance/files/controller.sh
+++ advance/files/controller.sh
@@ -151,14 +151,14 @@ cat $response_file | while read line ; do
 		else
 			uci set network.lan="interface"
 			uci set network.lan.proto="static"
-			uci set network.lan.ipaddr="172.16.99.1"
-			uci set network.lan.netmask="255.255.255.0"
+			uci set network.lan.ipaddr="192.168.173.1"
+			uci set network.lan.netmask="255.255.255.248"
 			uci set network.lan.type="bridge"
 			uci set network.lan.ifname="eth1.1"
 			uci set dhcp.lan.force="1"
-			uci set dhcp.lan.netmask="255.255.255.0"
+			uci set dhcp.lan.netmask="255.255.255.248"
 			uci del dhcp.lan.dhcp_option
-			uci add_list dhcp.lan.dhcp_option="6,8.8.8.8,8.8.4.4"				
+			uci add_list dhcp.lan.dhcp_option="6,192.168.173.199,192.168.177.199"				
 			uci set network.wan.ifname="eth0"
 		fi
 	#Cu hinh IP LAN
